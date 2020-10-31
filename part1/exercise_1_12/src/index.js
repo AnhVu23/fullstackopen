7b4fff9b276792d73c68c59eaa4f3860b505495a
Anh Vu <vu.haianh291@gmail.com>
2020-10-31 21:18:25 +0200

    Feat: Finish exercise 1.14

--- part1/exercise_1_12/src/index.js
+++ part1/exercise_1_12/src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick}>
+      <span>{text}</span>
+    </button>
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(anecdotes.map(dot => 0))
+
+  const onNextClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const onVoteClick = () => {
+    const cloneVotes = [...votes]
+    cloneVotes[selected] = cloneVotes[selected] + 1
+    setVotes(cloneVotes)
+  }
+  const highestVotes = Math.max(...votes)
+  const highestVotePosition = votes.indexOf(highestVotes)
+  return (
+    <div className='container'>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <div className='button-groups'>
+        <Button text='vote'
+              onClick={onVoteClick}
+        />
+        <Button text='next anecdote'
+              onClick={onNextClick}
+        />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[highestVotePosition]}</p>
+      <p>has {highestVotes} votes</p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
