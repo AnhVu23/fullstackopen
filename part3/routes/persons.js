520f798b0cad0a4cdb48ff36c6cffd7b78076922
Anh Vu <vu.haianh291@gmail.com>
2020-11-29 13:18:56 +0200

    Feat: Init part 3

---
+++ part3/routes/persons.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var router = express.Router()
+
+const personList = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+]
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send(personList)
+})
+
+module.exports = router
