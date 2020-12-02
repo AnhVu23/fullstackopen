8ffcde55be0819dfd3518e48406f00e547c0432b
Anh Vu <vu.haianh291@gmail.com>
2020-12-02 23:46:38 +0200

    Feat: Connect part of backend to database

--- part3/routes/persons.js
+++ part3/routes/persons.js
@@ -1,36 +1,16 @@
 const express = require('express')
 const { HttpError, BadRequest } = require('http-errors')
-var router = express.Router()
+const router = express.Router()
+const Person = require('../models/person')
 
-const personList = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-]
 /* GET users listing. */
 router
   .route('/')
-  .get(function (req, res, next) {
-    return res.send(personList)
+  .get(async function (req, res, next) {
+    const allPersons = await Person.find({})
+    return res.send(allPersons)
   })
-  .post(function (req, res, next) {
+  .post(async function (req, res, next) {
     try {
       if (!req.body.name) {
         throw new BadRequest('Name is missing')
@@ -38,18 +18,16 @@ router
       if (!req.body.number) {
         throw new BadRequest('Number is missing')
       }
-      const foundExistPerson = personList.find(person => person.name === req.body.name)
-      if (!foundExistPerson) {
-        throw new BadRequest('Name must be unique')
-      }
-      const newPerson = { ...req.body }
-      // Random an id with big enough range
-      newPerson.id =
-        Number(Math.random().toFixed(3)) * 1000 + personList.length + 1
-      personList.push(newPerson)
-      return res.header({ Location: `/api/persons/${newPerson.id}` }).send({
-        Location: `/api/persons/${newPerson.id}`,
+      // const foundExistPerson = personList.find(person => person.name === req.body.name)
+      // if (!foundExistPerson) {
+      //   throw new BadRequest('Name must be unique')
+      // }
+      const newPerson = new Person({
+        name: req.body.name,
+        number: req.body.number,
       })
+      const createdPerson = await newPerson.save()
+      return res.header({ Location: `/api/persons/${newPerson.id}` }).json(createdPerson)
     } catch (e) {
       return res.status(400).send(e)
     }
@@ -57,19 +35,15 @@ router
 
 router
   .route('/:id')
-  .get(function (req, res, next) {
-    const foundPerson = personList.find(
-      (person) => person.id === parseInt(req.params.id, 10)
-    )
+  .get(async function (req, res, next) {
+    const foundPerson = await Person.findById(parseInt(req.params.id, 10))
     if (!foundPerson) {
       return res.status(404).send()
     }
     return res.send(foundPerson)
   })
-  .delete(function (req, res, next) {
-    const foundPersonIndex = personList.findIndex(
-      (person) => person.id === parseInt(req.params.id, 10)
-    )
+  .delete(async function (req, res, next) {
+    const foundPerson = await Person.findById(parseInt(req.params.id, 10))
     if (foundPersonIndex === -1) {
       return res.status(404).send()
     }
