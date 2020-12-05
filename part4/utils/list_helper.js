1b29257f9be863ddf4d217b9182259382d04f03c
Anh Vu <vu.haianh291@gmail.com>
2020-12-05 21:30:36 +0200

    Feat: Add favorite blogs and most blogs functions and test functions for them

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
@@ -8,7 +9,32 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const sortedList = blogs.sort((curBlog, prevBlog) => prevBlog.likes - curBlog.likes)
+    return sortedList[0]
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return {
+            author: '',
+            blogs: 0
+        }
+    }
+    const countByAuthor = _.countBy(blogs, 'author')
+    const authorList = Object.keys(countByAuthor).map(key => ({
+        author: key,
+        blogs: countByAuthor[key]
+    })).sort((curAuthor, prevAuthor) => prevAuthor.blogs - curAuthor.blogs)
+    return authorList[0]
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
+    mostBlogs,
 }
--- part4/utils/list_helper.test.js
+++ part4/utils/list_helper.test.js
@@ -24,3 +24,119 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 2,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 3,
+      __v: 0,
+    },
+  ]
+
+  test('Normal test for the list has 3 blogs', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    })
+  })
+
+  test('when list has no blog, return null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(null)
+  })
+})
+
+describe('most blogs', () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Anh Vu',
+        url:
+          'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url:
+          'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url:
+          'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url:
+          'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Helsinki Open',
+        url:
+          'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 3,
+        __v: 0,
+      },
+    ]
+  
+    test('Test for a sample blogs', () => {
+      const result = listHelper.mostBlogs(blogs)
+      expect(result).toEqual({
+          author: 'Edsger W. Dijkstra',
+          blogs: 3
+      })
+    })
+  
+    test('when list has no blog, return author with empty string and number of blog is 0', () => {
+      const result = listHelper.mostBlogs([])
+      expect(result).toEqual({
+          author: '',
+          blogs: 0
+      })
+    })
+  })
