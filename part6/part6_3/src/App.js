1fa9ab68d141bc8906277b12c9657a49cc354864
Anh Vu <vu.haianh291@gmail.com>
2020-12-27 14:10:06 +0200

    Feat: Add notification when voting

--- part6/part6_3/src/App.js
+++ part6/part6_3/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {vote, createAnecdote} from './reducers/anecdoteReducer'
+import {vote, createAnecdote} from './reducers/anecdote'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Notification from './components/Notification'
+import { displayNotification, removeNotification } from './reducers/notification'
 
 const App = () => {
   const anecdotes = useSelector(state => state.anecdote)
@@ -10,6 +12,9 @@ const App = () => {
 
   const voteAnecdote = (id) => {
     dispatch(vote(id))
+    const foundAnec = anecdotes.find(anec => anec.id === id)
+    dispatch(displayNotification(`you voted '${foundAnec ? foundAnec.content : ''}'`))
+    setTimeout(() => dispatch(removeNotification()), 2000)
   }
 
   const onFormSubmit = event => {
@@ -21,6 +26,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification/>
       <h2>Anecdotes</h2>
       <AnecdoteList anecdotes={anecdotes} voteAnecdote={voteAnecdote}/>
       <AnecdoteForm onFormSubmit={onFormSubmit}/>
--- part6/part6_3/src/index.js
+++ part6/part6_3/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
 import store from './store'
 
 ReactDOM.render(
